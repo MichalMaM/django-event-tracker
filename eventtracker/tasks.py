3cfbd62897eeffcd4fcedf9345828719cddef5b6
MichalMaM <MichalMaM@centrum.cz>
2010-10-18 15:13:20 +0200

    collect events by Task class using delay without using direct carrot, refs #6605

--- eventtracker/tasks.py
+++ eventtracker/tasks.py
@@ -1,37 +1,11 @@
 from datetime import timedelta
 from time import time
 
-from celery.task import PeriodicTask
+from celery.task import Task
 from celery.registry import tasks
-from carrot.connection import DjangoBrokerConnection
-from carrot.messaging import Publisher, Consumer
 
 from eventtracker.conf import settings
 from eventtracker import models 
-
-publisher = None
-
-def _get_carrot_object(klass, **kwargs):
-    "Helper function to create Publisher and Consumer objects."
-    return klass(
-            connection=DjangoBrokerConnection(),
-            exchange=settings.EXCHANGE,
-            routing_key=settings.ROUTING_KEY,
-            exchange_type="topic",
-            **kwargs
-        )
-    
-def _close_carrot_object(carobj):
-    "Close Consumer or Publisher safely."
-    if carobj:
-        try:
-            carobj.close()
-        except:
-            pass
-        try:
-            carobj.connection.close()
-        except:
-            pass
 
 
 def track(event, params):
@@ -44,52 +18,35 @@ def track(event, params):
     a short-term network failure to disable one thread from commucating with
     the queue at the cost of retrying the connection every time.
     """
-    global publisher
-    if publisher is None:
-        # no connection or there was an error last time
-        # reinitiate the Publisher
-        publisher = _get_carrot_object(Publisher)
-
-    try:
-        # put the message into the queue including current time
-        publisher.send((event, time(), params))
-    except:
-        # something went wrong, probably a connection error or something. Close
-        # the carrot connection and set it to None so that the next request
-        # will try and reopen it.
-        _close_carrot_object(publisher)
-        publisher = None
-        raise
+   
+    ProcessEventTask.delay((event, time(), params))
 
 
-def collect_events():
+def collect_event(message):
     """
     Collect all events waiting in the queue and store them in the database.
     """
-    consumer = None
     collection = None
     try:
-        consumer = _get_carrot_object(Consumer, queue=settings.QUEUE)
         collection = models.get_mongo_collection()
-
-        for message in consumer.iterqueue():
-            e, t, p = message.decode()
-            models.save_event(collection, e, t, p)
-            message.ack()
+        e, t, p = message
+        models.save_event(collection, e, t, p)
+	print e,": ", datetime.now()
 
     finally:
-        _close_carrot_object(consumer)
         if collection:
             try:
                 collection.connection.close()
             except:
                 pass
 
-class ProcessEventsTask(PeriodicTask):
-    "Celery periodic task that collects events from queue."
-    run_every = timedelta(seconds=settings.TASK_PERIOD)
+class ProcessEventTask(Task):
+    "Celery task that collect event from queue."
+    name="eventtrackerrt.tasks.ProcessEventTask"
 
-    def run(self, **kwargs):
-        collect_events()
+    def run(self, message):
+	collect_event(message=message)
+	
 
-tasks.register(ProcessEventsTask)
+tasks.register(ProcessEventTask)
+
